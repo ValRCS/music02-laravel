732de6d895a788694a3c86d48334a58fc4982c24
Valdis Saules <valdis.s.coding@gmail.com>
2020-02-26 23:14:08 +0200

    Added Looping help

--- resources/views/music.blade.php
+++ resources/views/music.blade.php
@@ -10,8 +10,9 @@
     <p>My song  {{ $track->name }}</p>
     <p>My artist {{ $track->artist }}</p>
     <p>My album {{ $track->album }}</p>
+    <a href="https://laravel.com/docs/5.8/blade#php">Check Blade Docs</a><hr>
     @foreach ($track as $key=>$value)
-        <p>This {{$key}} has value: {{$value}}</p>
+        <p>Loop: {{$loop->index}} This {{$key}} has value: {{$value}}</p>
     @endforeach
     <hr>
 @php
